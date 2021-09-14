c5f79e6affd41582181f85ecacdebd863787a311
giddster <73110741+giddster@users.noreply.github.com>
2021-09-14 15:36:49 +0200

    Update SearchComponent.vue
    changed button to router-link

--- ljudia/src/components/SearchComponent.vue
+++ ljudia/src/components/SearchComponent.vue
@@ -2,7 +2,7 @@
     
     <form>
         <input type="text" class="searchbar" placeholder="Search for music..">
-        <button class="search-btn"> <i class="fas fa-search"></i> </button>
+        <router-link :to="'/search'" class="search-btn"> <i class="fas fa-search"> </i> </router-link>
     </form>
     
 </template>
@@ -25,10 +25,9 @@
 
 .search-btn{
     background: #22577a;
-    height: 3.4vh;
     color: white;
-    font-size: 17px;
-    border: 2px solid #22577a;
+    font-size: 2.5vh;
+    border: 1px solid #22577a;
     border-left: none; 
     cursor: pointer;
 }
