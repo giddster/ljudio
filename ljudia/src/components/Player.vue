6802dd1874e236d5329ca6734f5bf3a7406a19b8
giddster <73110741+giddster@users.noreply.github.com>
2021-09-15 10:30:29 +0200

    implementing player
    can now play example tracks, needs integration with existing design

--- ljudia/src/components/Player.vue
+++ ljudia/src/components/Player.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     
     <div class="miniplayer-parent">
         
@@ -46,7 +46,30 @@
 
     </div>
     
+</template> -->
+
+<template>
+  <div>
+    <button @click="play('DXxeOvvNNwc')">The Black Page #1 on piano</button>
+    <button @click="play('CtkZxnkbjtI')">The Black Page #2 live band</button>
+    <button @click="pause()">Pause</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods:{
+    play(id){
+      // calling global variable
+      window.player.loadVideoById(id)
+      window.player.playVideo()
+    },
+    pause(){
+      window.player.pauseVideo()
+    }
+  }
+}
+</script>
 
 <style scoped>
 
