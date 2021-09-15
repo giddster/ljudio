9cb1d6576c5c0c8b3d57a627fba6c52fde98758f
giddster <73110741+giddster@users.noreply.github.com>
2021-09-15 08:13:28 +0200

    Update FAQ.vue
    removed em tags in faq section

--- ljudia/src/views/FAQ.vue
+++ ljudia/src/views/FAQ.vue
@@ -6,49 +6,49 @@
             <summary>
                 <strong>Does this service cost money?</strong>
             </summary>
-            <em>No, infinimusic is completely free of charge! We don't even ask you to create an account to listen to your favorite music!</em>
+           No, infinimusic is completely free of charge! We don't even ask you to create an account to listen to your favorite music!
         </details>
 
         <details>
             <summary>
                 <strong>Can I stream music from my phone or tablet?</strong>
             </summary>
-            <em>Yes! Infinimusic is designed to scale with all device screens.</em>
+            Yes! Infinimusic is designed to scale with all device screens.
         </details>
 
         <details>
             <summary>
                 <strong>Can I share links to the music here with my friends and family?</strong>
             </summary>
-            <em>Yes, you can generate links for individual tracks, for albums and for artists.</em>
+            Yes, you can generate links for individual tracks, for albums and for artists.
         </details>
 
         <details>
             <summary>
                 <strong>I'm looking for very specific music. Will I find it here?</strong>
             </summary>
-            <em>Try searching for it from the search bar! Our music library is always expanding, but if you're missing something, send us an e-mail!</em>
+            Try searching for it from the search bar! Our music library is always expanding, but if you're missing something, send us an e-mail!
         </details>
 
         <details>
             <summary>
                 <strong>I'm experiencing bugs, what do I do?</strong>
             </summary>
-            <em>Please send us an e-mail and describe the bug as thoroughly as possible. Please attach screenshots if you can.</em>
+            Please send us an e-mail and describe the bug as thoroughly as possible. Please attach screenshots if you can.
         </details>
 
         <details>
             <summary>
                 <strong>Is this service even legal?</strong>
             </summary>
-            <em><a href="https://www.youtube.com/watch?v=nz20lu2AM2k" target="blank">I will make it legal</a></em>
+            <a href="https://www.youtube.com/watch?v=nz20lu2AM2k" target="blank">I will make it legal</a>
         </details>
 
         <details>
             <summary>
                 <strong>How did you make this?</strong>
             </summary>
-            <em>infinimusic is a web application made with VueJS, using the Heroku API to access the YouTube Music library.</em>
+            infinimusic is a web application made with VueJS, using the Heroku API to access the YouTube Music library.
         </details>
 
     </div>
