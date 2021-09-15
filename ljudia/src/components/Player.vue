8502cd27b9ad67b8ae82a8df43f8d8ad8f91a3ee
giddster <73110741+giddster@users.noreply.github.com>
2021-09-15 09:35:40 +0200

    Update Player.vue
    trying grid design instead

--- ljudia/src/components/Player.vue
+++ ljudia/src/components/Player.vue
@@ -1,75 +1,43 @@
 <template>
     
-    <div class="parent">
+    <div class="miniplayer-parent">
         
-        <div class="div1"> 
+        <div class="miniplayer-metadata"> 
             <strong>Now playing</strong>
             <p>Never Gonna Give You Up</p>
             <p>Rick Astley</p>
         </div>
 
-        <div class="div2"> 
-            <i class="fas fa-step-backward previousbutton mini-buttons"></i>
-            <i class="fas fa-play playbutton mini-buttons"></i>
+        <div class="miniplayer-buttons"> 
+            <i class="fas fa-step-backward mini-buttons" id=""></i>
+            <i class="fas fa-play mini-buttons"></i>
             <i class="fas fa-pause mini-buttons"></i>
-            <i class="fas fa-step-forward nextbutton mini-buttons"></i>
+            <i class="fas fa-step-forward mini-buttons"></i>
+            
         </div>
 
-        <div class="div3"> 
+        <div class="miniplayer-volume"> 
             <i class="fas fa-volume-up mini-buttons"></i>
             <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
             <!-- <i class="fas fa-volume-down mini-buttons">less than 50%</i>
             <i class="fas fa-volume-mute mini-buttons">mute</i> -->
         </div>
 
-        <div class="div4"> 
+        <div class="miniplayer-expander"> 
             <i class="fas fa-angle-double-up mini-buttons"></i> Expand player
         </div>
 
     </div>
     
-    <!-- <div class="player-minified">
-        
-        <div class="mini-metadata">
-            <strong>Now playing</strong>
-            <p>Never Gonna Give You Up</p>
-            <p>Rick Astley</p>
-        </div>
-            
-        
-
-        <div class="mini-playerbuttons">
-             
-            <i class="fas fa-step-backward previousbutton mini-buttons"></i>
-            <i class="fas fa-play playbutton mini-buttons"></i>
-            <i class="fas fa-pause mini-buttons"></i>
-            <i class="fas fa-step-forward nextbutton mini-buttons"></i>
-            <p>+ PROGRESS BAR</p>
-            
-        </div>
-
-        <div class="mini-volumearea">
-            
-            <i class="fas fa-volume-up mini-buttons"></i>
-            <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
-            <i class="fas fa-volume-down mini-buttons">less than 50%</i>
-            <i class="fas fa-volume-mute mini-buttons">mute</i>
-        </div>
-
-        <div class="mini-expandtoggle">
-            <i class="fas fa-angle-double-up mini-buttons"></i> Expand player
-        </div>
-
-    </div> -->
 </template>
 
 <style scoped>
 
-.parent {
+.miniplayer-parent {
     display: grid;
     align-items: center;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: 0.5fr;
+    grid-template-columns: 1fr 2fr 0.5fr 0.5fr;
+    grid-template-rows: 1fr;
     grid-column-gap: 0px;
     grid-row-gap: 0px;
     background: #38a3a5c9;
@@ -79,39 +47,28 @@
     bottom: 8.2vh;
 }
 
-.div1 { grid-area: 1 / 1 / 2 / 2; }
-.div2 { grid-area: 1 / 2 / 2 / 3; }
-.div3 { grid-area: 1 / 3 / 2 / 4; }
-.div4 { grid-area: 1 / 4 / 2 / 5; }
-.player-minified{
-    background: #38a3a5c9;
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 100vw;
-    height: 10vh;
-    bottom: 8.2vh;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    outline: 1px solid black;
-    padding: 1vh 1vw 0 1vw;
+.miniplayer-buttons, .miniplayer-volume, .miniplayer-expander {
+    display: grid;
+    grid-auto-flow: column;
+    align-content: center;
+    justify-content: center;
 }
 
-.mini-metadata{
-    width: 20%;
+.miniplayer-metadata { 
+    grid-area: 1 / 1 / 2 / 2;
+    margin-left: 1vw;
 }
-
-.mini-playerbuttons{
-    width: 50%;
+.miniplayer-buttons { 
+    grid-area: 1 / 2 / 2 / 3; 
+    
 }
+.miniplayer-volume { 
+    grid-area: 1 / 3 / 2 / 4; 
 
-.mini-volumearea{
-    width: 20%;
 }
+.miniplayer-expander { 
+    grid-area: 1 / 4 / 2 / 5; 
 
-.mini-expandtoggle{
-    width: 20%;
 }
 
 .mini-buttons{
