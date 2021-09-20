13bfeb844fe2743ef6294d4f51d1a026e11e43dc
giddster <73110741+giddster@users.noreply.github.com>
2021-09-20 13:18:22 +0200

    Update SearchResults.vue
    now displaying both songs and artists

--- ljudia/src/views/SearchResults.vue
+++ ljudia/src/views/SearchResults.vue
@@ -1,21 +1,47 @@
 <template>
-    <h1 class="results-heading"> Your results for "{{ searchString }}"</h1>
-    <!-- <h1 v-else> Sorry, we couldn't find results for "{{ searchString }}"</h1> -->
-    
-    <div class="all-results" v-for="result in searchResults" :key="result">
-            
-            <div v-if="result.type == 'song' || result.type == 'album'" class="single-result">
-                <p>Title: {{result.name}}</p> 
-                <p>Artist: {{result.artist.name}}</p>
-                <p>Album: {{result.album.name}}</p>
-                <p>Type: {{result.type}}</p>
+    <div v-if="searchResults.length" class="results-container">
+        <h1 class="results-heading"> Your results for "{{ searchString }}"</h1>
+        
+        <div class="sort-buttons">
+            <p>Filter results by </p>
+            <button>All</button>
+            <button>Songs</button>
+            <button>Artists</button>
+        </div>
 
-                <button @click="loadSongToPlayer(result)"> <i class="fas fa-play mini-buttons" id="mini-playbutton"></i> </button>
-                <button> <i class="fas fa-bars"></i> </button>
-                
-            </div>
+        <div class="all-results" v-for="result in searchResults" :key="result">
+
+                <div v-if="result.type == 'song'" class="single-result">
+                    <div class="result-metadata">
+                        <img :src="result.thumbnails[1].url" alt="">
+                        <p> Title: {{result.name}}</p> 
+                        <p>Artist: {{result.artist.name}}</p>
+                        <p>Album: {{result.album.name}}</p>
+                    </div>
+
+                    <div class="result-buttons">
+                        <button id="result-playbutton" @click="loadSongToPlayer(result)"> <i class="fas fa-play mini-buttons" id="mini-playbutton"></i> Play song </button>
+                        <button id="result-queuebutton"> <i class="fas fa-plus"></i> Add to queue </button>
+                    </div>
+                </div>
+
+                <div v-else-if="result.type == 'artist'" class="single-result">
+                    <div class="result-metadata">
+                        <img :src="result.thumbnails[1].url" alt="">
+                        <p>{{result.name}}</p> 
+                    </div>
+
+                    <div class="result-buttons">
+                        <button id="result-playbutton"> <i class="fas fa-id-badge"></i> Visit artist profile </button>
+                    </div>
+                    
+                </div>
+        </div>
     </div>
-    
+
+    <div v-else class="no-results">
+            <h1> Sorry, we couldn't find any results for "{{ searchString }}"</h1>
+    </div>
 </template>
 
 <script>
@@ -39,15 +65,73 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+.results-container{
+    background: #acf1d4b7;
+    padding: 0.5vh 0;
+    min-height: fit-content;
+}
+
 .results-heading{
     text-align: center;
     font-size: 3vh;
     font-family: 'IBM Plex Sans Arabic', sans-serif;
 }
 
+.sort-buttons{
+    display: flex;
+    justify-content: center;
+}
+
 .all-results{
-    width: 85vw;
+    margin: 0 auto;
+    padding: 0.5vh 1vw;
+}
+
+.single-result{
+    margin: 0 auto;
+    padding: 1vh 1vw;
+    width: 80%;
+    display: flex;
+    font-family: 'IBM Plex Sans Arabic', sans-serif;
+    justify-content: center;
+    align-items: center;
+}
+
+.single-result p{
+    padding: 0;
+    margin: 5px;
+    color: #22577a;
+}
+
+.result-metadata{
+    width: 50%;
+}
+
+.result-buttons{
+    width: 50%;
+    text-align: center;
+}
+
+.result-buttons button{
+    font-size: 3vh;
+    padding: 0 1vw;
+    background: #38a3a500;
+    border: none;
+    cursor: pointer;
+}
+
+.result-buttons button:active{
+    zoom: 1.1;
+    background-color: #3ed9db;
+}
+
+#result-playbutton{
+    color: #57cc99;
+}
+
+#result-queuebutton{
+    color: #22577a;
 }
 
 </style>
